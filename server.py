bb17dcfae835222e813419b0e533d65f6dc0f049
karthiksuki <karthikeyangan05@gmail.com>
2026-09-18 21:29:12 +0530

    tcp broadcast func

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
 import socket   # Netowrk connections 
 import threading # To perform vartious tasks at the same time 
 
+# Configuration
+host = "localhost" 
+port = 5500 
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server.bind((host, port))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+
+def broadcast(msg): 
+    for client in clients: 
+        client.send(msg) 
 
 
 
