7acd2b79514f8f27471c0d63706f06c1d102a262
karthiksuki <karthikeyangan05@gmail.com>
2026-09-18 14:16:32 +0530

    add: Initial commit for tcp_chat

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from tcp-chat!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,7 @@
+import socket   # Netowrk connections 
+import threading # To perform vartious tasks at the same time 
+
+
+
+
+
