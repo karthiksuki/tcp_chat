c8d7ec0c1d64ae72bd1f0522d24926620f9b72bb
karthiksuki <karthikeyangan05@gmail.com>
2026-09-18 22:49:31 +0530

    handle method and connetion for tcp server

--- server.py
+++ server.py
@@ -7,7 +7,6 @@ port = 5500
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 server.bind((host, port))
-
 server.listen()
 
 clients = []
@@ -17,6 +16,37 @@ nicknames = []
 def broadcast(msg): 
     for client in clients: 
         client.send(msg) 
+
+
+def handle(client): 
+    while True: 
+        try: 
+            msg = client.recv(1024)
+            broadcast(msg)
+        except: 
+            index = clients.index(client)
+            clients.remove(client)
+            client.close() 
+            nicknames = nicknames[index]
+            broadcast('{} left!'.format(nicknames).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        client.send('NAME'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
 
 
 
